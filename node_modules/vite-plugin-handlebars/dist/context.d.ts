fe986ea27f72170e54e3cd2d052bddf4209f66dd
Archi Gene <44780361+mrzenxxx@users.noreply.github.com>
2023-06-19 20:19:35 +0300

    Revert "replacing files up to root of repository"
    This reverts commit 2f6db86e88cb966fce963f2a0684c7e5cb73434d.

--- node_modules/vite-plugin-handlebars/dist/context.d.ts
+++
@@ -1,2 +0,0 @@
-export declare type Context = Record<string, unknown> | ((path: string) => Record<string, unknown>) | ((path: string) => Promise<Record<string, unknown>>);
-export declare function resolveContext(context: Context | undefined, pagePath: string): Promise<Record<string, unknown> | undefined>;
--- node_modules/vite-plugin-handlebars/dist/partials.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Registers each HTML file in a directory as Handlebars partial
- */
-export declare function registerPartials(directoryPath: string | Array<string>, partialsSet: Set<string>): Promise<void>;
