5fcfced3baee7d4a14768dd1df1138f86c787134
Archi Gene <44780361+mrzenxxx@users.noreply.github.com>
2023-09-25 20:56:26 +0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = 3001;
+const PORT = 3000;
 const app = express();
 
 app.use(express.static(path.join(`${__dirname}/dist`)));
