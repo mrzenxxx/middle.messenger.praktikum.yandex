9781c6b521137f36e2bfa521594f94e39ff78ea6
Archi Gene <44780361+mrzenxxx@users.noreply.github.com>
2023-10-30 21:09:03 +0300

    Update Block.test.ts

--- src/core/test/Block.test.ts
+++ src/core/test/Block.test.ts
@@ -5,46 +5,46 @@ import Block from '../Block';
 interface DummyProps extends StringIndexed { }
 
 class Dummy extends Block<DummyProps> {
-  constructor(props: DummyProps) {
-    super({ ...props });
-  }
+    constructor(props: DummyProps) {
+        super({ ...props });
+    }
 
-  render() {
-    return this.compile('<button>Test Text<button>', {});
-  }
+    render() {
+        return this.compile('<button>Test Text<button>', {});
+    }
 }
 
 const block = new Dummy({
-  testProps: { key: 'value' },
+    testProps: { key: 'value' },
 });
 
 describe('Block', () => {
-  it('Must return correct tag name', () => {
-    assert.equal(block.element!.tagName, 'BUTTON');
-  });
-
-  it('Must return correct content', () => {
-    assert.equal(block.element!.textContent, 'Test Text');
-  });
-
-  it('Must change it\'s props', () => {
-    block.setProps({ ...block.props, id: 'testId' });
-    assert.deepEqual(block.props, { id: 'testId', testProps: { key: 'value' } });
-  });
-
-  it('Must handling events from props', () => {
-    const testHandleEvent: SinonStub = stub();
-    const testEvent = new MouseEvent('click');
-
-    block.setProps({
-      ...block.props,
-      events: {
-        click: testHandleEvent,
-      },
+    it('Must return correct tag name', () => {
+        assert.equal(block.element!.tagName, 'BUTTON');
     });
+
+    it('Must return correct content', () => {
+        assert.equal(block.element!.textContent, 'Test Text');
+    });
+
+    it('Must change it\'s props', () => {
+        block.setProps({ ...block.props, id: 'testId' });
+        assert.deepEqual(block.props, { id: 'testId', testProps: { key: 'value' } });
+    });
+
+    it('Must handling events', () => {
+        const testHandleEvent: SinonStub = stub();
+        const testEvent = new MouseEvent('click');
+
+        block.setProps({
+            ...block.props,
+            events: {
+                click: testHandleEvent,
+            },
+        });
         block.element!.dispatchEvent(testEvent);
 
         // eslint-disable-next-line
         expect(testHandleEvent.calledOnce).to.be.true;
-  });
+    });
 });
